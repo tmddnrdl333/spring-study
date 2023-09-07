df303965a55910626a6860e1d2af57ed80e8d5eb
SeungNong <tmddnrdl333@naver.com>
2023-09-07 21:47:15 +0900

    feat: H2 db 연동

--- seungnong/src/main/java/summer/seungnong/SpringConfig.java
+++ seungnong/src/main/java/summer/seungnong/SpringConfig.java
@@ -1,14 +1,25 @@
 package summer.seungnong;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import summer.seungnong.repository.JdbcMemberRepository;
 import summer.seungnong.repository.MemberRepository;
-import summer.seungnong.repository.MemoryMemberRepository;
 import summer.seungnong.service.MemberService;
+
+import javax.sql.DataSource;
 
 @Configuration
 
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
@@ -16,6 +27,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
